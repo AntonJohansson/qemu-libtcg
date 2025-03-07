fb13cfb75a57bd2037da52116002dc7c5ce996cd
Anton Johansson <anjo@rev.ng>
2025-03-07 16:47:12 +0100

    fix dummy.h
    Signed-off-by: Anton Johansson <anjo@rev.ng>

--- dummy.h
+++ dummy.h
@@ -12,11 +12,8 @@ void cpu_loop_exit_sigbus(CPUState *cpu, target_ulong addr,
 void cpu_loop_exit_sigsegv(CPUState *cpu, target_ulong addr,
                            MMUAccessType access_type, bool maperr, uintptr_t ra);
 bool have_mmap_lock(void);
-//void *lock_user(int type, abi_ulong guest_addr, ssize_t len, bool copy);
 void mmap_lock(void);
 void mmap_unlock(void);
-//bool qemu_cpu_is_self(CPUState *cpu);
-//void qemu_cpu_kick(CPUState *cpu);
 void qemu_plugin_add_dyn_cb_arr(GArray *arr);
 void qemu_plugin_flush_cb(void);
 void qemu_plugin_tb_trans_cb(CPUState *cpu, void *tb);
