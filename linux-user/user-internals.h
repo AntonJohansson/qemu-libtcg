9636d1c6e60212e0c16f6a669f255c2c6bb9f188
Anton Johansson <anjo@rev.ng>
2025-03-07 14:24:55 +0100

    user-internals

--- linux-user/user-internals.h
+++ linux-user/user-internals.h
@@ -22,7 +22,9 @@
 #include "exec/exec-all.h"
 #include "exec/tb-flush.h"
 #include "qemu/log.h"
-#include <linux/mman.h>
+#ifdef GEN_LLVM_HELPERS
+  #include <linux/mman.h>
+#endif
 
 extern char *exec_path;
 void init_task_state(TaskState *ts);
